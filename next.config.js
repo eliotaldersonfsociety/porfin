60c2d8f46a835f98e9787e3853bb683ec1806b5a
eliotaldersonfsociety <aldersonelliot803@gmail.com>
2025-03-13 02:55:03 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,7 @@
-// next.config.js
-module.exports = {
-  // Remove swcMinify if present; enabled by default
-  webpack: (config) => {
-    // Only customize if necessary
-    config.optimization.splitChunks = {
-      cacheGroups: {
-        vendors: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all',
-          enforce: true,
-        },
-      },
-    };
-    return config;
-  },
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
+
+export default nextConfig;
